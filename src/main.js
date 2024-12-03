9d2b468bebf955e3822bdd2808f64c362fbd64c8
Nevio Werlen <LordNevi@outlook.com>
2024-12-03 08:43:05 +0100

    add Inputs

--- src/main.js
+++ src/main.js
@@ -15,6 +15,14 @@ $(".generate-modal").each(function() {
     $(this).append(HTMLContent);
 });
 
+/**
+ * Generates the HTML content for a modal dialog.
+ *
+ * @param {Object} params - The parameters for the modal.
+ * @param {string} params.id - The ID of the modal.
+ * @param {string} params.title - The title of the modal.
+ * @returns {string} The HTML string for the modal.
+ */
 function getHTMLContent(params) {
     const content = getModalContent(params);
     const footer = getFooter(params);
@@ -152,11 +160,20 @@ function getNormalButton(params) {
     return deleteButton;
 }
 
+/**
+ * Generates the HTML content for a modal based on the provided parameters.
+ *
+ * @param {Object} params - The parameters for generating the modal content.
+ * @param {Object} [params.form] - The form configuration object. If provided, a form will be generated.
+ * @param {Array} params.inputs - An array of input configurations to be included in the modal.
+ * @param {string} [params.content] - Additional HTML content to be included in the modal.
+ * @returns {string} The generated HTML content for the modal.
+ */
 function getModalContent(params) {
     let HTML = ``;
 
     if (params.form != undefined){
-        HTML += getForm(params.form);
+        HTML += getForm(params.form, params.inputs);
     }else{
         HTML += getInputs(params.inputs);
     }
@@ -169,7 +186,17 @@ function getModalContent(params) {
     return HTML;
 }
 
-function getForm(params) {
+/**
+ * Generates an HTML form string based on the provided parameters and inputs.
+ *
+ * @param {Object} params - The parameters for the form.
+ * @param {string} [params.id="form"] - The ID of the form.
+ * @param {string} [params.action="#"] - The action URL of the form.
+ * @param {string} [params.method="POST"] - The HTTP method of the form.
+ * @param {Array} inputs - An array of input elements to be included in the form.
+ * @returns {string} The generated HTML form string.
+ */
+function getForm(params, inputs) {
     if (params.id == undefined){
         params.id = "form";
     }
@@ -184,17 +211,68 @@ function getForm(params) {
 
     const form = `
         <form id="${params.id}" action="${params.action}" method="${params.method}">
-            ${getInputs(params.inputs)}
+            ${getInputs(inputs)}
         </form>
     `;
 
     return form;
 }
 
+/**
+ * Generates HTML for a list of input parameters.
+ *
+ * @param {Array} [params] - An array of input parameters. Each parameter is expected to be an object that can be processed by the `getInput` function.
+ * @returns {string} The generated HTML string. If no parameters are provided, returns a message indicating no inputs were provided.
+ */
 function getInputs(params) {
+    var HTML = ``;
+
     if (params == undefined){
-        return `No inputs provided.`;
+        HTML = `No inputs provided.`;
+    }else{
+        for (let i = 0; i < params.length; i++) {
+            const input = params[i];
+            HTML += getInput(input);
+        }
     }
 
-    // Todo: generate Inputs
+    return HTML;
+}
+
+/**
+ * Generates an HTML input element with the specified parameters.
+ *
+ * @param {Object} params - The parameters for the input element.
+ * @param {string} [params.id="input"] - The id of the input element.
+ * @param {string} [params.type="text"] - The type of the input element.
+ * @param {string} [params.label="Label"] - The label for the input element.
+ * @param {string} [params.placeholder="Enter text here"] - The placeholder text for the input element.
+ * @returns {string} The HTML string for the input element.
+ */
+function getInput(params) {
+    if (params.id == undefined){
+        params.id = "input";
+    }
+
+    if (params.type == undefined){
+        params.type = "text";
+    }
+
+    if (params.label == undefined){
+        params.label = "Label";
+    }
+
+    if (params.placeholder == undefined){
+        params.placeholder = "Enter text here";
+    }
+
+
+    const input = `
+        <div class="mb-3">
+            <label for="${params.id}" class="form-label">${params.label}</label>
+            <input type="${params.type}" class="form-control" id="${params.id}" placeholder="${params.placeholder}">
+        </div>
+    `;
+
+    return input;
 }
